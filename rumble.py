f057d92840423c1738d1988929b9d5e0dd17fac5
Abhishek Yadav <abhiyd2401@gmail.com>
2023-07-22 21:26:05 +0530

    first commit

---
+++ rumble.py
@@ -0,0 +1,23 @@
+
+# used sliding window approach to find max in each array window size		
+def rumbling(nums, X):
+    maximum = []
+    first, second = 0, X
+    maximum.append(max(nums[first:second])) 
+    
+    while second < len(nums):
+        maximum.append(max(nums[first: second+1]))
+        first += 1
+        second += 1
+        
+    return maximum
+
+# test case 1
+nums1 = [1, 3, -1, -3, 5, 3, 6, 7]
+X = 3
+print(rumbling(nums1, X))
+
+# test case 2
+nums2 = [1, 2, -1, 2, 4, 5]
+Y = 2
+print(rumbling(nums2, Y))
